0409c376681261013af6bddf42f263e3d0ad4301
alohama <iknest@gmail.com>
2021-05-10 16:57:43 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 const { i18n } = require('./next-i18next.config')
 
 module.exports = {
-    future:{
-      webpack5:true  
-    },
-    i18n,
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback.fs = false;
-        }
-        return config;
-    },
+    i18n
 }
